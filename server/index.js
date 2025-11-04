1c7f811eb9938aa6b737e70a17a8582303b99cd2
Rajkumar Mali <rajkumarmali@Rajkumars.local>
2025-11-04 20:05:11 +0530

    db connection

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
-console.log("This is server")
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const app = express();
+const PORT = 3001;
+
+const DbConnection = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://curd:curd@cluster0.sh6mfsu.mongodb.net/?appName=Cluster0');
+        console.log("MongoDb connected")
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send("Hello world");
+})
+
+DbConnection();
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
